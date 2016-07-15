59e701099e445c2ebde339db5403926673f88826
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-15 22:53:20 +0200

    Added facebook login

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -67,7 +67,7 @@ window.fbAsyncInit = function()
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "http://connect.facebook.net/en_US/all.js";
+      js.src = "https://connect.facebook.net/en_US/all.js";
      fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
