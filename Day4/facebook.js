475455ff85fa965dfccc4a85f252267d9216af1b
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-16 18:34:04 +0200

    testing facebook API

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -79,7 +79,7 @@ app.controller("MainCtrl", function($scope, $routeParams)
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
