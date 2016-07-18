a5d04b863c4953e4bb7b1dae748ba1649b3dc61f
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-18 15:29:42 +0200

    working with faceook API

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -71,7 +71,7 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
   {
       $scope.albumID = response.data[1].id;
       console.log(response.data[1].id);
-      FB.api('/'+$scope.albumID+'/photos?fields=data,picture', function(response) 
+      FB.api('/'+$scope.albumID+'/photos.limit(500)?fields=data,picture', function(response) 
       {
         console.log(response);
       });
