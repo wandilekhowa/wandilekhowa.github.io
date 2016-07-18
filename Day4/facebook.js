a5f1512422234321dc741363f79f4d99c043b745
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-18 14:14:03 +0200

    working with faceook API

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -54,6 +54,7 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
   // }
   FB.api('/me?fields=id,name,cover,hometown,about,bio,gender,languages,link,locale,location,updated_time,timezone,work', function(response) 
   {
+      $scope.userName = response.name;
       console.log(response);
       //window.location.href = "/#/username/userID/"+response.name+"/"+response.id;
       // console.log('Successful login for: ' + response.name);
