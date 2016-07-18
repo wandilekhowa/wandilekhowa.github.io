b63afc76e9539d168e20bcf02e888ced5a844a4e
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-18 12:44:10 +0200

    working with faceook API

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -43,11 +43,12 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
   $scope.userName = $routeParams.user_name;
   $scope.userId = $routeParams.user_id;
   console.log($routeParams.user_name+" "+$routeParams.user_id);
-  FB.api('/me?fields=id,about,bio,name,email,quotes', function(response) 
+  FB.api('/me?fields=id,about,bio,name,email,quotess', function(response) 
   {
       console.log(response);
       $scope.bio = response.bio;
       $scope.name = response.name;
+      $scope.email = response.email;
       //window.location.href = "/#/username/userID/"+response.name+"/"+response.id;
       // console.log('Successful login for: ' + response.name);
       // document.getElementById('status').innerHTML =
