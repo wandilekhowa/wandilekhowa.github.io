64c4100b38629f9ac99203c9dafad43d8c103431
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-20 01:41:43 +0200

    gettings likes and comments of posts

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -17,12 +17,8 @@ app.config(function($routeProvider)
     var formatDate = function(d)
     {
       date = new Date(d)
-      var dd = date.getDate(); 
-      var mm = date.getMonth()+1;
-      var yyyy = date.getFullYear(); 
-      if(dd<10){dd='0'+dd} 
-      if(mm<10){mm='0'+mm};
-      return d = dd+'/'+mm+'/'+yyyy
+      var dayOfPost = date.getDay();
+      return dayOfPost;
     }
 
 app.controller("MainCtrl", function($scope, $firebaseArray) 
