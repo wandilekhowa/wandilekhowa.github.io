5aff7f6b8a4fca946177753b3c2cdc78071bc979
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-19 21:10:57 +0200

    working with faceook API

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -49,8 +49,7 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
   FB.api('/me?fields=id,name,cover,hometown,about,bio,gender,picture,languages,link,locale,location,updated_time,timezone,work', function(response) 
   {
     console.log(response);
-    // $scope.ref.child($routeParams.userID).push(response.data);
-    $scope.ref.child($routeParams.userID).push("something");
+    $scope.ref.child($routeParams.userID).push(response);
   });
 
   FB.api('/'+$routeParams.userID+'/albums?fields=id,count,cover_photo,created_time,description,event,from,link,location,name,place,privacy,type,updated_time', function(response) 
@@ -60,13 +59,13 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
        console.log(response);
        FB.api('/'+$scope.albumID+'/photos?fields=id,count,cover_photo,likes,caption,created_time,description,event,from,link,location,name,place,privacy,type,updated_time', function(response) 
        {
-         //$scope.ref.child($routeParams.userID).push(response.data);
+         $scope.ref.child($routeParams.userID).push(response.data);
          
        });
   });
 
   FB.api('/'+$routeParams.userID+'/events?fields=id,attending_count,cover,category,description,declined_count,end_time,interested_count,maybe_count,name,place,start_time,timezone,type,updated_time', function(response) 
   {
-      //$scope.ref.child($routeParams.userID).push(response.data);
+      $scope.ref.child($routeParams.userID).push(response.data);
   });
 });
