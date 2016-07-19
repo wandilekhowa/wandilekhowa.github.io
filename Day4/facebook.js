b01ccc3c4c231b565694bb5362f757e740ac2fd4
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-19 14:24:53 +0200

    working with faceook API

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -56,16 +56,59 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
   //   var twitter = new Twitter(config);
   //   twitter.getSearch({'q':'#capetown','count': 100}, error, success);
 
-    var consumerKey = encodeURIComponent('b21Mq7VV3sOKnvupl87ZatVei')
-    var consumerSecret = encodeURIComponent('aWU5msqZ47zmivi5MF5rrdhdzfEvZqIEx0d7yIjYtNw2vsadLN');
-    var credentials = (consumerKey + ':' + consumerSecret);
+    var authorizationResult = false;
 
-// Twitters OAuth service endpoint
-var twitterOauthEndpoint = $http.get(
-  'https://api.twitter.com/oauth2/3039716836-PrmDqeMfsMhW4ZMiRG0k8fHmbaC6coEMk7SKKEW'
-  , "grant_type=client_credentials"
-  , {headers: {'Authorization': 'Basic ' + credentials, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}}
-);
+    return {
+        initialize: function() {
+            //initialize OAuth.io with public key of the application
+            OAuth.initialize('19gVB-kbrzsJWQs5o7Ha2LIeX4I', {
+                cache: true
+            });
+            //try to create an authorization result when the page loads,
+            // this means a returning user won't have to click the twitter button again
+            authorizationResult = OAuth.create("twitter");
+        },
+        isReady: function() {
+            return (authorizationResult);
+        },
+        connectTwitter: function() {
+            var deferred = $q.defer();
+            OAuth.popup("twitter", {
+                cache: true
+            }, function(error, result) {
+                // cache means to execute the callback if the tokens are already present
+                if (!error) {
+                    authorizationResult = result;
+                    deferred.resolve();
+                } else {
+                    //do something if there's an error
+
+                }
+            });
+            return deferred.promise;
+        },
+        clearCache: function() {
+            OAuth.clearCache('twitter');
+            authorizationResult = false;
+        },
+        getLatestTweets: function(maxId) {
+            //create a deferred object using Angular's $q service
+            var deferred = $q.defer();
+            var url = '/1.1/statuses/home_timeline.json';
+            if (maxId) {
+                url += '?max_id=' + maxId;
+            }
+            var promise = authorizationResult.get(url).done(function(data) {
+                // https://dev.twitter.com/docs/api/1.1/get/statuses/home_timeline
+                // when the data is retrieved resolve the deferred object
+                deferred.resolve(data);
+            }).fail(function(err) {
+                deferred.reject(err);
+            });
+            //return the promise of the deferred object
+            return deferred.promise;
+        }
+    }
 
   // $scope.userName = $routeParams.userObj.name;
   // $scope.home = $routeParams.userObj.location.name;
