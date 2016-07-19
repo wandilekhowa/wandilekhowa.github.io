f7a1b4ecdee89140ddd5df4ee5f3bf141404f276
Wandile Khowa <khwwan001@myuct.ac.za>
2016-07-20 00:17:48 +0200

    sorting facebook profile objects

--- Day4/facebook.js
+++ Day4/facebook.js
@@ -100,15 +100,9 @@ app.controller("ProfileCtrl", function($scope, $http ,$routeParams, $firebaseArr
       $scope.ref.child($routeParams.userID).child("Events").push(response.data);
   });
 
-  FB.api('/'+$routeParams.userID+'/posts?metadata=1', function(response) 
+  FB.api('/'+$routeParams.userID+'/posts?fields=likes,comments', function(response) 
   {
       console.log("fetching feed");
-      console.log(response);
-      //$scope.ref.child($routeParams.userID).child("Events").push(response.data);
-  });
-  FB.api('/'+$routeParams.userID+'/photos/uploaded?fields=likes,comments', function(response) 
-  {
-      console.log("fetching pics");
       console.log(response);
       //$scope.ref.child($routeParams.userID).child("Events").push(response.data);
   });
